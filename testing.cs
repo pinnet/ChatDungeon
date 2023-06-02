848282ef00e53477ab8b6ac153a5aa5a9788867b
pinnet <danny@dannyarnold.com>
2023-06-02 08:30:46 +0100

    Installed nuget packegs

---
+++ testing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using DotNetEnv;
+
+public class Program
+{
+    public static void Main()
+    {
+        Env.Load(); // Loads the environment variables from the .env file
+
+        string apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+        Console.WriteLine(apiKey);
+    }
+}
